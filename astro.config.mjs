a256d529b25df54e1b86cf577cc14f12aa5e1eb8
redstonekasi <kasi@hanneskann.de>
2022-09-13 01:22:49 +0200

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  // hello yes this should actually be kasi.dev but somebody bought
+  // that in the exact same moment i decided that i wanted it
+  site: "https://k6.tf"
+});

a7a2abd2171c01c400e5a5b33fb2ffd4b09270ea
redstonekasi <kasi@hanneskann.de>
2022-11-05 18:59:48 +0100

    add sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://k6.tf",
   markdown: {
-    remarkPlugins: [
-      () => (tree, file) => {
-        file.data.astro.frontmatter.layout = "@layouts/Post.astro";
-      }
-    ],
+    remarkPlugins: [() => (tree, file) => {
+      file.data.astro.frontmatter.layout = "@layouts/Post.astro";
+    }]
   },
+  integrations: [sitemap()]
 });
